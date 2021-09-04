27be7c7c7e2edb6d4ddad9b0c409f6e375cbd1cb
Souvik Biswas <sbis1999@gmail.com>
2021-09-04 18:25:37 +0530

    Current deployed function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,55 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const SecretManagerServiceClient = require("@google-cloud/secret-manager");
+const StreamChat = require("stream-chat").StreamChat;
+admin.initializeApp();
+
+// async function accessSecretVersion(secretKeyName) {
+//     const [secretVersion] = await secretClient.accessSecretVersion({
+//         name: secretKeyName,
+//     });
+//     const secret = secretVersion.payload.data.toString();
+//     return secret;
+// }
+
+exports.newUserSignUp = functions.auth.user().onCreate((user) => {
+  const timestamp = admin.firestore.FieldValue.serverTimestamp();
+  admin.firestore().collection("users").doc(user.uid).set({
+    uid: user.uid,
+    email: user.email,
+    displayName: user.displayName,
+    photoURL: user.photoURL,
+    createdAt: timestamp,
+  });
+
+  console.log("New user created: ", user.uid);
+});
+
+exports.getStreamToken = functions.https.onCall((data, context) => {
+  const userID = data.uid;
+  const secretClient = new SecretManagerServiceClient();
+
+  const [streamAPIKeyVersion] = secretClient.accessSecretVersion({
+    name: "stream_api_key",
+  });
+
+  const [streamAPISecretVersion] = secretClient.accessSecretVersion({
+    name: "stream_api_secret",
+  });
+
+  const streamAPIKey = streamAPIKeyVersion.payload.data.toString();
+  const streamAPISecret = streamAPISecretVersion.payload.data.toString();
+
+  // const streamAPIKey = await accessSecretVersion('stream_api_key');
+  // const streamAPISecret = await accessSecretVersion('stream_api_secret');
+
+  const streamClient = new StreamChat(streamAPIKey, streamAPISecret);
+  const token = streamClient.createToken(userID);
+
+  console.log("Stream token generated for user: ", userID);
+
+  return token;
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
