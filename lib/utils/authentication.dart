36669ee88c43225cbf1378122c672bc6cfd0c93e
Souvik Biswas <sbis1999@gmail.com>
2021-11-06 14:10:26 +0530

    Update authentication logic

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -1,8 +1,15 @@
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:stream_auth_firebase/screens/channel_list_page.dart';
+import 'package:stream_auth_firebase/secrets.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart' as sc;
 
 class Authentication {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFunctions functions = FirebaseFunctions.instance;
+  User? firebaseUser;
+
   static SnackBar customSnackBar({required String content}) {
     return SnackBar(
       backgroundColor: Colors.black,
@@ -13,11 +20,71 @@ class Authentication {
     );
   }
 
-  FirebaseAuth auth = FirebaseAuth.instance;
-  FirebaseFunctions functions = FirebaseFunctions.instance;
-  User? firebaseUser;
+  _initializeStream({
+    required BuildContext context,
+    required String token,
+    required User user,
+  }) async {
+    final client = sc.StreamChatClient(
+      streamKey,
+      logLevel: sc.Level.OFF,
+    );
 
-  Future<String?> signInUsingEmailPassword({
+    await client.connectUser(
+      sc.User(
+        id: user.uid,
+        extraData: {
+          'name': user.displayName,
+          'image': user.photoURL,
+        },
+      ),
+      token,
+    );
+
+    final channel = client.channel('messaging', id: 'general');
+    await channel.watch();
+
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+      builder: (context) => MaterialApp(
+        builder: (context, widget) {
+          return sc.StreamChat(
+            child: widget,
+            client: client,
+          );
+        },
+        debugShowCheckedModeBanner: false,
+        home: ChannelListPage(channel),
+      ),
+    ));
+  }
+
+  Future<bool> isSignedIn(BuildContext context) async {
+    firebaseUser = auth.currentUser;
+    bool isSignedIn = false;
+
+    if (firebaseUser != null) {
+      isSignedIn = true;
+      try {
+        final callable = functions.httpsCallable('getStreamUserToken');
+        final results = await callable();
+        String? token = results.data;
+
+        if (token != null) {
+          _initializeStream(
+            context: context,
+            token: token,
+            user: firebaseUser!,
+          );
+        }
+      } catch (e) {
+        print('Error in fetching token: $e');
+      }
+    }
+
+    return isSignedIn;
+  }
+
+  Future<void> signInUsingEmailPassword({
     required String email,
     required String password,
     required BuildContext context,
@@ -33,6 +100,16 @@ class Authentication {
       final callable = functions.httpsCallable('getStreamUserToken');
       final results = await callable();
       token = results.data;
+
+      if (token != null) {
+        print('Stream token retrieved (signed in)');
+
+        _initializeStream(
+          context: context,
+          token: token,
+          user: firebaseUser!,
+        );
+      }
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
@@ -50,18 +127,14 @@ class Authentication {
         );
       }
     }
-
-    return token;
   }
 
-  Future<String?> registerUsingEmailPassword({
+  Future<void> registerUsingEmailPassword({
     required String name,
     required String email,
     required String password,
     required BuildContext context,
   }) async {
-    String? token;
-
     try {
       UserCredential userCredential = await auth.createUserWithEmailAndPassword(
         email: email,
@@ -69,13 +142,28 @@ class Authentication {
       );
 
       firebaseUser = userCredential.user;
-      await firebaseUser!.updateDisplayName(name);
-      await firebaseUser!.reload();
-      firebaseUser = auth.currentUser;
+
+      if (firebaseUser != null) {
+        await firebaseUser!.updateDisplayName(name);
+        await firebaseUser!.reload();
+        firebaseUser = auth.currentUser;
+      } else {
+        throw ('Firebase user is null');
+      }
 
       final callable = functions.httpsCallable('createStreamUserAndGetToken');
       final results = await callable();
-      token = results.data;
+      String? token = results.data;
+
+      if (token != null) {
+        print('Stream token retrieved (registered)');
+
+        _initializeStream(
+          context: context,
+          token: token,
+          user: firebaseUser!,
+        );
+      }
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         print('The password provided is too weak.');
@@ -95,8 +183,6 @@ class Authentication {
     } catch (e) {
       print(e);
     }
-
-    return token;
   }
 
   Future<void> signOut() async {
